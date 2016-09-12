78f6af5cfaec7445cc43425a6290672bad04b6cd
lee <lee@clearlovedeMacBook-Pro.local>
2016-09-12 16:40:36 +0800

    u

---
+++ web.js
@@ -0,0 +1,16 @@
+var http = require('http')
+var url = require('url')
+var queryString = require('querystring')
+var request = require('request')
+var cheerio = require("cheerio")
+var promise = require('request-promise')
+
+http.createServer(function(req,resq){
+	var bookname=queryString.parse(url.parse(req.url).query).n
+	
+	resq.setHeader('Content-Type','text/html;charset=utf8')
+	//resq.write("<style>*{color:#999}a{color:#009;}</style>")
+	resq.end("i love guojianli");
+
+}).listen(process.env.PORT || 3000)
+
