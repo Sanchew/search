70ff5d9e60cef44ff4f96082c8c1f039879b9f48
lee <lee@clearlovedeMacBook-Pro.local>
2016-09-21 16:02:22 +0800

    u

---
+++ mapping.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var router = express.Router()
+
+router.use('/', function(req,res,next){
+  if(req.url == '/'){
+    var referer = req.headers.referer
+    var path = referer.match(/http:\/\/\w+.\w+(.*$)/)[1]
+    if (path.length > 0) {
+	req.url = path
+	var query = path.split('?')
+	if (query.length > 1) {
+	    req.query = queryString.parse(query[1])
+	}
+    }
+  }
+    next()
+})
+
+module.exports = router
