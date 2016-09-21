4f05403aa194a4de760fc968319ff2fadcdc9521
lee <lee@clearlovedeMacBook-Pro.local>
2016-09-21 16:01:55 +0800

    u

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express')
 var favicon = require('serve-favicon')
 var queryString = require('querystring')
 var findbook = require('./searchbook')
-
+var mapping = require('./mapping')
 // var http = require('http')
 // var url = require('url')
 // var queryString = require('querystring')
@@ -15,26 +15,13 @@ var app = express();
 // app.engine('html',require('ejs').renderFile)
 // app.set('view engine','html')
 app.use(favicon(__dirname + '/public/favicon.ico'))
-app.use('/', function(req,res,next){
-  if(req.url == '/'){
-    var referer = req.headers.referer
-    var path = referer.match(/http:\/\/\w+.\w+(.*$)/)[1]
-    if (path.length > 0) {
-	req.url = path
-	var query = path.split('?')
-	if (query.length > 1) {
-	    req.query = queryString.parse(query[1])
-	}
-    }
-  }
-    next()
-})
+app.use('/',mapping)
 app.use('/static',express.static('public'))
 app.use('/fb',findbook)
 app.get('/',function(req,res){
     res.send('等待是一场与时光的较量')  
 })
-app.get('/love',function(req,res){
+app.get('/guojianli',function(req,res){
     res.redirect('/static/loveyou/mylove.html')
     // res.sendFile('/public/loveyou/mylove.html')
 })
