23f04a3f0e14dd50cf0317290f16108b2c325c90
lee <lee@clearlovedeMacBook-Pro.local>
2016-09-21 16:09:01 +0800

    u

--- mapping.js
+++ mapping.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.use('/', function(req,res,next){
-    if(req.url == '/'){
+    if(req.url != '/'){
         var referer = req.headers.referer
         var path = referer.match(/http:\/\/\w+.\w+(.*$)/)[1]
         if (path.length > 0) {
