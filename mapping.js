0494a17238d7196a287f7904761a5f9721cd3469
lee <lee@clearlovedeMacBook-Pro.local>
2016-09-21 16:19:29 +0800

    u

--- mapping.js
+++ mapping.js
@@ -5,15 +5,9 @@ router.use('/', function(req,res,next){
     if(req.url == '/'){
         var referer = req.headers.referer
         var path = referer.match(/http:\/\/\w+.\w+(.*$)/)[1]
-        if (path.length > 0 && path != '/') {
-            req.url = path
-	    console.info(`redirect ${path}`)
+        if (path.length > 1) {
 	    res.redirect(path)
 	    return
-            var query = path.split('?')
-            if (query.length > 1) {
-                req.query = queryString.parse(query[1])
-            }
         }
     }
     next()
