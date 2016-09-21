da33267c0193c35ae2af679588c3cddc4339bbf4
lee <lee@clearlovedeMacBook-Pro.local>
2016-09-21 10:06:23 +0800

    u

--- web.js
+++ web.js
@@ -8,33 +8,30 @@ var findbook = require('./searchbook')
 // var promise = require('request-promise')
 var app = express();
 // app.use(express.static('public'))
+// app.set('views',__dirname)
+// app.engine('html',require('ejs').renderFile)
+// app.set('view engine','html')
+app.use(function(req,res,next){
+    console.info(`ref = ${req.referer}`)
+    if (req.referer) {
+	console.info(`${req.url} ${req.referer}`)
+	//req.url = req.referer
+    }
+    next()
+})
 app.use('/static',express.static('public'))
 app.use('/fb',findbook)
 app.get('/',function(req,res){
-    // res.send('guojianli')
-    res.redirect('/static/loveyou/mylove.html')
+    console.info(req.url)
+    res.send('等待是一场与时光的较量')  
 })
-
 app.get('/love',function(req,res){
-    res.send('郭')
+    res.redirect('/static/loveyou/mylove.html')
+    // res.sendFile('/public/loveyou/mylove.html')
 })
 var server = app.listen(process.env.PORT || 3000,function(){
     var host = server.address().address,
 	port = server.address().port
     console.log('app listening at http://%s:%s',host,port)
 })
-// http.createServer(function(req,resq){
-// 	var bookname=queryString.parse(url.parse(req.url).query).n
-// 	
-// 	resq.setHeader('Content-Type','text/html;charset=utf8')
-// 	//resq.write("<style>*{color:#999}a{color:#009;}</style>")
-// 	request("https://raw.githubusercontent.com/lijianwei-jj/heroku/master/README.md",function(e,res,body){
-// 		console.log(body)
-// 		console.log("request end")
-// 		resq.end(body)
-// 	})
-// 	console.log("what?")
-// 	// resq.end("i love guojianli");
-// 
-// }).listen(process.env.PORT || 3000)
 
