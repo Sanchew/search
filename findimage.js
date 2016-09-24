e464aae52975161b318064895f5f3fec2be35581
lee <lijianei.jj@gmail.com>
2016-09-24 12:27:51 +0800

    u'

--- findimage.js
+++ findimage.js
@@ -18,12 +18,13 @@ router.get('/',function(req,resq){
 		var detailUrl = `https://www.google.com/ajax/pi/imgdisc?imgdii=uc9uNt3w_TH2OM`
 		var url = `http://www.google.com.hk/search?async=_id:rg_s,_pms:s&q=kingboo&asearch=ichunk&tbm=isch&ijn=${page}&gws_rd=cr`
 		var body = yield corequest(murl(url))
-		console.info(body)
+		// console.info(body)
 		var dom = JSON.parse(body.body)[1][1]
 		var $=cheerio.load(dom)//,{decodeEntities:false})
 		// console.info(dom)
 		var jsons = $('.rg_meta')
 		for (var i=0;i<jsons.length;i++) {
+			console.info(jsons.eq(i).html())
 			var json = JSON.parse(jsons.eq(i).html())
 			resq.write(`<img src="${json.ou}">`)
 		}
